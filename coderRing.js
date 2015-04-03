7f4739d060bcb6d12d19a139cf7b8d7670f530c6
Kit <lcramer6@gmail.com>
2015-04-03 10:35:15 -0400

    Added exception for negative shift values for vowels.

--- coderRing.js
+++ coderRing.js
@@ -6,6 +6,8 @@
 var vowels = ["a", "e", "i", "o", "u", "y"];
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
 
+//Tests whether letter is a vowel
+
 var vowelTest = function(ltr) {
     for (var j = 0; j <= vowels.length - 1; j++) {
 	if (ltr === vowels[j]) {
@@ -14,24 +16,28 @@ var vowelTest = function(ltr) {
 	}
 };
 
+//Changes the letter
+
 var coderRing = function(oldLtr, shft){
 	
 	var newLtr = "";
+//account for negative shift values
+	var vShft = (shft < 0 ? shft+vowels.length : shft);
+	
 	
 	if (vowelTest(oldLtr) === true) {
-	    if ((vowels.indexOf(oldLtr) + shft) <= vowels.length - 1) {
-		    newLtr = vowels[vowels.indexOf(oldLtr) + shft];
+	    if ((vowels.indexOf(oldLtr) + vShft) <= vowels.length - 1) {
+		    newLtr = vowels[vowels.indexOf(oldLtr) + vShft];
 	    }
 	    else {
 			vowels = vowels.concat(vowels);
-		    newLtr = vowels[vowels.indexOf(oldLtr) + shft];
+		    newLtr = vowels[vowels.indexOf(oldLtr) + vShft];
 	    }
 	}
 	
 	else {
 		newLtr = consonants[consonants.indexOf(oldLtr) + shft];
 	}
-	
 	return newLtr;
 };
 
@@ -42,6 +48,7 @@ var secretWord = function(msg, shft) {
 	        if (oldMsg.charAt(i) === " ") {
 	            newMsg = newMsg + " ";
 	        }
+
 	        else {
     	        var change = oldMsg.charAt(i);
 	            newMsg = newMsg + coderRing(change, shft);
@@ -51,4 +58,4 @@ var secretWord = function(msg, shft) {
 };
 
 console.log(secretWord("Yes", 2));
-console.log(secretWord("I am Kit", 1));
+console.log(secretWord("I am Kit", -1));
